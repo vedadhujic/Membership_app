4cfb95bbf301dd0c8d729eba19b249f084a0c07b
Vedad Hujić <hujicvedad@outlook.com>
2019-09-13 01:04:33 +0200

    Entities

---
+++ Membership_app/Entities/ProductLinkText.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Membership_app.Entities
+{
+    [Table("ProductLinkText")]
+    public class ProductLinkText
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(25)]
+        [Required]
+        public string Title { get; set; }
+    }
+}
