b3f66bb216ab1c60b01f2386fa52cec3d404a491
Vedad Hujić <hujicvedad@outlook.com>
2020-03-05 22:50:11 +0100

    UserSubscriptionViewModel

---
+++ Membership_app/Models/UserSubscriptionModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Membership_app.Models
+{
+    public class UserSubscriptionModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        [MaxLength(255)]
+        public string Description { get; set; }
+
+        [MaxLength(20)]
+        public string RegistrationCode { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+    }
+}
---
+++ Membership_app/Models/UserSubscriptionViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Membership_app.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Membership_app.Models
+{
+    public class UserSubscriptionViewModel
+    {
+        public ICollection<Subscription> Subscriptions { get; set; }
+
+        public ICollection<UserSubscriptionModel> UserSubscriptions { get; set; }
+
+        public bool DisableDropDown { get; set; }
+
+        public string UserId { get; set; }
+
+        public int SubscriptionId { get; set; }
+    }
+}
